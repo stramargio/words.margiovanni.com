f764e71913bcae6157be1748e7b3c9588f41a11a
Andrea Margiovanni <andrea@margiovanni.com>
2023-02-16 10:57:15 +0100

    primi due articoli

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,9 +5,6 @@ $( document ).ready(function() {
 	var dropcaps = document.querySelectorAll(".dropcap");
 	window.Dropcap.layout(dropcaps, 2);
 
-	// Responsive-Nav
-	var nav = responsiveNav(".nav-collapse");
-
 	// Round Reading Time
     $(".time").text(function (index, value) {
       return Math.round(parseFloat(value));
